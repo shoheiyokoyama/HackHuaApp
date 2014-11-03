c58a22a39cf6c1db25cf48f79637b20112eb8ed6
Shohei Yokoyama <shohei.yok0602@gmail.com>
2014-11-03 10:07:52 +0900

    Initial Commit

---
+++ HackHuaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackHuaApp
+//
+//  Created by 横山祥平 on 2014/11/03.
+//  Copyright (c) 2014年 Hua. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
