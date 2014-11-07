a8879befb02513edac2f5d219cd808ef1bb48e8d
Shohei Yokoyama <shohei.yok0602@gmail.com>
2014-11-07 23:27:18 +0900

    add
    加速度センサの値に応じて、画像とテキストを出力

--- HackHuaApp/ViewController.swift
+++ HackHuaApp/ViewController.swift
@@ -8,11 +8,28 @@
 
 import UIKit
 import CoreMotion
+import AVFoundation
+struct AVAudioPlayerUtil {
+    
+    static var audioPlayer:AVAudioPlayer = AVAudioPlayer();
+    static var sound_data:NSURL = NSURL();
+    
+    static func setValue(nsurl:NSURL){
+        self.sound_data = nsurl;
+        self.audioPlayer = AVAudioPlayer(contentsOfURL: self.sound_data, error: nil);
+        self.audioPlayer.prepareToPlay();
+    }
+    static func play(){
+        self.audioPlayer.play();
+    }
+}
+
 
 class ViewController: UIViewController {
 
     var myMotionManager: CMMotionManager!
-    
+  
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +37,7 @@ class ViewController: UIViewController {
         
         
         // Labelを作成.
-        let myXLabel: UILabel = UILabel(frame: CGRectMake(0,0,300,50))
+        let myXLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
         myXLabel.backgroundColor = UIColor.grayColor()
         myXLabel.layer.masksToBounds = true
         myXLabel.layer.cornerRadius = 10.0
@@ -47,27 +64,85 @@ class ViewController: UIViewController {
         myZLabel.textAlignment = NSTextAlignment.Center
         myZLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 360)
         
+        let myWLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
+        myWLabel.backgroundColor = UIColor.greenColor()
+        myWLabel.layer.masksToBounds = true
+        myWLabel.layer.cornerRadius = 10.0
+        myWLabel.textColor = UIColor.whiteColor()
+        myWLabel.shadowColor = UIColor.grayColor()
+        myWLabel.textAlignment = NSTextAlignment.Center
+        myWLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 420)
+        
+
+        
         // Viewの背景色を青にする.
         self.view.backgroundColor = UIColor.cyanColor()
         
         // ViewにLabelを追加.
+       
         self.view.addSubview(myXLabel)
         self.view.addSubview(myYLabel)
         self.view.addSubview(myZLabel)
+        self.view.addSubview(myWLabel)
         
         // MotionManagerを生成.
         myMotionManager = CMMotionManager()
         
         // 更新周期を設定.
         myMotionManager.accelerometerUpdateInterval = 0.1
+       
         
         // 加速度の取得を開始.
         myMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
+            
+            let w  = accelerometerData.acceleration.x + accelerometerData.acceleration.y + accelerometerData.acceleration.z//xyz軸の加速度の合計
+            let a = "migi"
+            let b = "hidari"
+            let img:UIImage! = UIImage(named:"j1.jpeg")//画像
+            let img2:UIImage! = UIImage(named:"j2.jpeg")
+   
+            var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+            var sound_data = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("uaboy", ofType: "wav")!)
+            audioPlayer = AVAudioPlayer(contentsOfURL: sound_data, error: nil);
+            audioPlayer.prepareToPlay();
+           
+            
+            //テキストラベルに　xyz　の加速度の値を代入
             myXLabel.text = "x=\(accelerometerData.acceleration.x)"
             myYLabel.text = "y=\(accelerometerData.acceleration.y)"
             myZLabel.text = "z=\(accelerometerData.acceleration.z)"
+            
+          
+//            もしx軸の正の方向に動かした時
+            if (accelerometerData.acceleration.x > 0.1 )
+            {
+                let iv:UIImageView = UIImageView(image:img)
+                iv.frame = CGRectMake(100,100, 300, 300)
+                self.view.addSubview(iv)
+                myWLabel.text = "w=\(a)"//テキストにa = "migi"を代入
+                AVAudioPlayerUtil.play();//再生
+                
+                           }
+//                x軸マイナスの値に動かした時
+            else if (accelerometerData.acceleration.x < -0.1 )
+            {
+                let iv:UIImageView = UIImageView(image:img2)
+                iv.frame = CGRectMake(50,50, 100, 100)
+                self.view.addSubview(iv)
+                myWLabel.text = "w=\(b)"//テキストにb = "hidari"を表示
+                
+                
+                            }
         })
+       
         
+       
+       
+//        
+//        //停止について
+//        if (myMotionManager.accelerometerActive) {
+//            myMotionManager.stopAccelerometerUpdates()
+//        }
         
     }
 
@@ -78,4 +153,3 @@ class ViewController: UIViewController {
 
 
 }
-
