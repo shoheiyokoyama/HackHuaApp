0b165567ae8f6330a617e6c6d6b7f8b896016ed3
Shohei Yokoyama <shohei.yok0602@gmail.com>
2014-11-10 15:03:36 +0900

    add
    sound data add

--- HackHuaApp/MainViewController.swift
+++ HackHuaApp/MainViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 import CoreMotion
+import AVFoundation
 
 class MainViewController: UIViewController {
 
     var myMotionManager: CMMotionManager!
+    var audio00 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("uaboy", ofType: "wav")!)
+    var playeruaboy = AVAudioPlayer()
+    
+    var audio01 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kyaa1", ofType: "wav")!)
+    var playerkyaa1 = AVAudioPlayer()
+    var audio02 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("ohayo01", ofType: "mp3")!)
+    var playerohayo = AVAudioPlayer()
+    var audio03 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("uu", ofType: "wav")!)
+    var playeruu = AVAudioPlayer()
+    var audio04 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kyaa02", ofType: "mp3")!)
+    var playerkyaa02 = AVAudioPlayer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let img1:UIImage! = UIImage(named:"j1.jpeg")
+        let img2:UIImage! = UIImage(named:"j2.jpeg")
+        let img3:UIImage! = UIImage(named:"migi1.jpg")
+        let img4:UIImage! = UIImage(named:"hidari.jpg")
+        let img5:UIImage! = UIImage(named:"ue.jpg")
+        let img6:UIImage! = UIImage(named:"sita.jpg")
+        let img7:UIImage! = UIImage(named:"gori1.jpg")
+        let img8:UIImage! = UIImage(named:"gori2.jpg")
 
-        // Do any additional setup after loading the view.
-//       self.view.backgroundColor = UIColor.whiteColor()
+        playeruaboy = AVAudioPlayer(contentsOfURL: audio00, error: nil)
+        playeruaboy.prepareToPlay()
+        playerkyaa1 = AVAudioPlayer(contentsOfURL: audio01, error: nil)
+        playerkyaa1.prepareToPlay()
+        playerohayo = AVAudioPlayer(contentsOfURL: audio02, error: nil)
+        playerohayo.prepareToPlay()
+        playeruu = AVAudioPlayer(contentsOfURL: audio03, error: nil)
+        playeruu.prepareToPlay()
+        playerkyaa02 = AVAudioPlayer(contentsOfURL: audio04, error: nil)
+        playerkyaa02.prepareToPlay()
+        
+        playerohayo.currentTime = 0
+        playerohayo.play()
+
         
         let myWLabel: UILabel = UILabel(frame: CGRectMake(0,0,200,50))
         myWLabel.backgroundColor = UIColor.blueColor()
@@ -29,68 +62,61 @@ class MainViewController: UIViewController {
         myWLabel.layer.position = CGPoint(x: self.view.bounds.width/2,y: 420)
         
         self.view.addSubview(myWLabel)
+        let iv:UIImageView = UIImageView(image:img8)
+        iv.frame = CGRectMake(30,30, 300, 300)
+        self.view.addSubview(iv)
+        
         
         // MotionManagerを生成.
         myMotionManager = CMMotionManager()
         
         // 更新周期を設定.
-        myMotionManager.accelerometerUpdateInterval = 0.01
+        myMotionManager.accelerometerUpdateInterval = 0.2
         
         
         // 加速度の取得を開始.
         myMotionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.mainQueue(), withHandler: {(accelerometerData:CMAccelerometerData!, error:NSError!) -> Void in
+            
             
             let w  = accelerometerData.acceleration.x + accelerometerData.acceleration.y + accelerometerData.acceleration.z//xyz軸の加速度の合計
             let a = "→"
             let b = "←"
             let c = "↑"
             let d = "↓"
-            let img1:UIImage! = UIImage(named:"j1.jpeg")
-            let img2:UIImage! = UIImage(named:"j2.jpeg")
-            let img3:UIImage! = UIImage(named:"migi1.jpg")
-            let img4:UIImage! = UIImage(named:"hidari.jpg")
-            let img5:UIImage! = UIImage(named:"ue.jpg")
-            let img6:UIImage! = UIImage(named:"sita.jpg")
             
             
-            if (accelerometerData.acceleration.x > 0.1 )
+            
+            if (accelerometerData.acceleration.x > 1)
             {
-                let iv:UIImageView = UIImageView(image:img3)
-                iv.frame = CGRectMake(100,100, 100, 100)
+                self.playeruu.currentTime = 0
+                self.playeruu.play()
+
+                let iv:UIImageView = UIImageView(image:img7)
+                iv.frame = CGRectMake(30,30, 300, 300)
                 self.view.addSubview(iv)
                 myWLabel.text = "w=\(a)"
             }
                 
-            else if (accelerometerData.acceleration.x < -0.1 )
+            else if (w < 1)
             {
-                let iv:UIImageView = UIImageView(image:img4)
-                iv.frame = CGRectMake(100,100, 100, 100)
+                
+                let iv:UIImageView = UIImageView(image:img8)
+                iv.frame = CGRectMake(30,30, 300, 300)
                 self.view.addSubview(iv)
                 myWLabel.text = "w=\(b)"
                
                 
                 
             }
-            else if (accelerometerData.acceleration.y > 0.1 )
-            {
-                let iv:UIImageView = UIImageView(image:img5)
-                iv.frame = CGRectMake(100,100, 100, 100)
-                self.view.addSubview(iv)
-                myWLabel.text = "w=\(c)"
+            
                 
                 
                 
-            }
-            else if (accelerometerData.acceleration.y < -0.1 )
-            {
-                let iv:UIImageView = UIImageView(image:img6)
-                iv.frame = CGRectMake(100,100, 100, 100)
-                self.view.addSubview(iv)
-                myWLabel.text = "w=\(d)"
                 
-                
-                
-            }
+            
+            
+            
+           
         })
         
         
--- HackHuaApp/SoundViewController.swift
+++ HackHuaApp/SoundViewController.swift
@@ -12,27 +12,38 @@ import AVFoundation
 class SoundViewController: UIViewController {
 
 
-    let audioPath = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("uaboy", ofType: "wav")!)
-    var player = AVAudioPlayer()
+    var audio00 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("uaboy", ofType: "wav")!)
+    var playeruaboy = AVAudioPlayer()
+    
+    
+    var audio01 = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("kyaa1", ofType: "wav")!)
+    var playerkyaa1 = AVAudioPlayer()
+    
+    
   
     @IBAction func playbtn(sender: UIButton) {
-        player.play()
+        playeruaboy.currentTime = 0
+        playeruaboy.play()
     }
     @IBAction func pause(sender: UIButton) {
-        player.pause()
+        playerkyaa1.currentTime = 0
+        playerkyaa1.play()
     }
-    @IBAction func stop(sender: UIButton) {
-        player.stop()
-        player.currentTime = 0 //曲を頭に戻す
-    }
+//    @IBAction func stop(sender: UIButton) {
+//        player.stop()
+//        player.currentTime = 0 //曲を頭に戻す
+//    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        playeruaboy = AVAudioPlayer(contentsOfURL: audio00, error: nil)
+        playeruaboy.prepareToPlay()
         
+        playerkyaa1 = AVAudioPlayer(contentsOfURL: audio01, error: nil)
+        playerkyaa1.prepareToPlay()
         
-        player = AVAudioPlayer(contentsOfURL: audioPath, error: nil)
-        player.prepareToPlay()//プレイヤーを再生する準備　再生した後スムーズに曲ができる
+       
     }
 
     override func didReceiveMemoryWarning() {
